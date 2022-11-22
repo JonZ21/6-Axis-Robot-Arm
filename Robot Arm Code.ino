8a0f6cca9e1f40d6d9e28498911e340719f45b8c
Jonathan Zhou <Jonathanzhou8@gmail.com>
2022-11-21 21:46:20 -0500

    Added reset and speed control

--- Robot Arm Code.ino
+++ Robot Arm Code.ino
@@ -31,7 +31,12 @@ Servo claw;
 // joystick
 const int JOYSTICK_X_PORT = A0; // x axis analog input port
 const int JOYSTICK_Y_PORT = A1; // y axis analog input port
+const int JOYSTICK_BUTTON_PORT = 2;
 const int BUTTON_PORT = 3;
+const int BUTTON_S_UP_PORT = 12;
+const int BUTTON_S_DOWN_PORT = 13;
+
+
 int xPosition = 0;
 int yPosition = 0;
 
@@ -42,16 +47,23 @@ int mapY = 0;
 
 int index = 0; // index for switching between modes
 int buttonState = 0; 
+int bUState = 0;
+int bDState = 0;
+int joystickState = 0;
+double speedRatio = 0.0;
+bool bUPressing = false;
+bool bDPressing = false;
 bool pressing = false; 
+bool joystickPressing = false;
 
 //arrays containing the x and y values for each mode
-int xa[3]; 
-int ya[3];
+int xa[3] = {90,90,90}; 
+int ya[3] = {90,90,90};
 
 const int JOYSTICK_DEADZONE = 20; 
 const int JOYSTICK_MID_VALUE = 512;
 const int JOYSTICK_MAX_VALUE = 1023;
-const int JOYSTICK_RATE = 20; // a higher joystick value means the joystick is more sensitive
+const int JOYSTICK_RATE = 5; // a higher joystick value means the joystick is more sensitive
 void setup() {
  
   Serial.begin(9600); 
@@ -59,6 +71,7 @@ void setup() {
   //Joystick 
   pinMode(JOYSTICK_X_PORT, INPUT);
   pinMode(JOYSTICK_Y_PORT, INPUT);
+  pinMode(JOYSTICK_BUTTON_PORT, INPUT_PULLUP); 
 
   //servos
   base.attach(5);
@@ -74,13 +87,15 @@ void setup() {
   elbow.write(90);
   wristRoll.write(90);
   wristPitch.write(90);
-  claw.write(80); 
+  claw.write(90); 
 }
 
 void loop() {
   xPosition = analogRead(JOYSTICK_X_PORT);
   yPosition = analogRead(JOYSTICK_Y_PORT);
   buttonState = digitalRead(BUTTON_PORT);
+  joystickState = digitalRead(JOYSTICK_BUTTON_PORT);
+  
 
   if(abs(JOYSTICK_MID_VALUE - xPosition) > JOYSTICK_DEADZONE) {
     mapX = map(xPosition, 0, JOYSTICK_MAX_VALUE,-JOYSTICK_RATE,JOYSTICK_RATE); // map the value of x from 0 - 1023(JOYSTICK_MAX_VALUE) to +- Joystick rate
@@ -103,6 +118,21 @@ void loop() {
   if(ya[index] < 0)
     ya[index] = 0;
 
+  }
+
+  if(joystickState == HIGH) // reset
+    joystickPressing = true;
+  if(joystickPressing && joystickState != HIGH)
+  {
+    xa[0] = xa[1] = xa[2] = 90;
+    ya[0] = ya[1] = ya[2] = 90;
+    base.write(xa[1]);  
+    shoulder.write(ya[1]);
+    elbow.write(xa[1]);  
+    wristRoll.write(ya[1]);
+    wristPitch.write(xa[1]);  
+    claw.write(ya[1]);
+    joystickPressing = false;
   }
 
   if(buttonState == HIGH)
@@ -131,5 +161,5 @@ void loop() {
     claw.write(ya[index]);
   }
 
-  delay(100);
+  delay(50);
 }
